0022757f47ca7376a4fadd7a582caf54f555db0c
paulduchesne <paulduchesne@tuta.io>
2025-06-27 19:18:48 +1000

    comparison process

--- README.md
+++ README.md
@@ -1,6 +1,24 @@
 # King James Validator
 
-An interesting lyrical constraint which occured to me a short time ago, to use only words found in the King James translation of the Bible. 
+An interesting artistic constraint occured to me a short time ago, to use only words found in the King James translation of the Bible. 
+
+This validator produces a short report containing a percentage score of total words which overlap and a percentage of unique words which overlap, against a source text.
+
+**Example**
+
+```sh
+python3 kjv.py jane_austen.txt
+```
+
+returns the following report
+
+```sh
+{
+    'text': 'jane_austen.txt', 
+    'total_score': 0.86, 
+    'unique_score': 0.24
+}
+```
 
 **License**
 
--- kjv.py
+++ kjv.py
@@ -3,21 +3,9 @@ import pathlib
 import pydash
 import re
 import requests
+import sys
 
-kjv_words = pathlib.Path.cwd() / 'words.json'
-if not kjv_words.exists():
-
-    # pull text from gutenberg.
-
-    r = requests.get('https://www.gutenberg.org/cache/epub/10/pg10.txt')
-    if r.status_code != 200:
-        raise Exception('API call failed.')
-
-    # remove header and footer.
-
-    t = r.text
-    t = t.split('*** START OF THE PROJECT GUTENBERG EBOOK THE KING JAMES VERSION OF THE BIBLE ***')[1]
-    t = t.split('*** END OF THE PROJECT GUTENBERG EBOOK THE KING JAMES VERSION OF THE BIBLE ***')[0]
+def text_normalise(t):
 
     # convert linebreaks to spaces.
 
@@ -31,14 +19,75 @@ if not kjv_words.exists():
     # cast lowercase and remove any non-alpha characters.
 
     t = [x.lower() for x in t]
-    t = [re.sub(r'[^a-zA-Z]', '',x)  for x in t]
+    t = [re.sub(r'[^a-zA-Z]', '', x)  for x in t]
+
+    # sort and remove empty strings.
+
+    t = sorted([x for x in t if x])
+
+    return t
+
+kjv = pathlib.Path.cwd() / 'words.json'
+if not kjv.exists():
+
+    # pull from gutenberg.
+
+    r = requests.get('https://www.gutenberg.org/cache/epub/10/pg10.txt')
+    if r.status_code != 200:
+        raise Exception('API call failed.')
+
+    # remove header and footer.
+
+    t = r.text
+    t = t.split('*** START OF THE PROJECT GUTENBERG EBOOK THE KING JAMES VERSION OF THE BIBLE ***')[1]
+    t = t.split('*** END OF THE PROJECT GUTENBERG EBOOK THE KING JAMES VERSION OF THE BIBLE ***')[0]
+
+    # normalise.
+
+    t = text_normalise(t)
 
     # unique words only, and sort.
 
     t = pydash.uniq(t)
     t = sorted([x for x in t if x])
 
-    # render word list.
+    # export word list.
 
-    with open(kjv_words, 'w') as words_out:
+    with open(kjv, 'w') as words_out:
         json.dump({'words':t}, words_out, indent=4)
+
+# load kjv word array.
+
+with open(kjv) as kjv_words:
+    kjv_words = json.load(kjv_words)['words']
+
+# load comparison text.
+
+input_file = pathlib.Path.cwd() / sys.argv[1]
+if not input_file.exists():
+    raise Exception('Input file not found.')
+
+with open(input_file) as input_text:
+    input_text = input_text.read()
+
+word_array = text_normalise(input_text)
+
+# determine score for all words.
+
+a = [(x in kjv_words) for x in word_array]
+total_score = round((sum(a)/len(a)), 2)
+
+# determine score for unique words.
+
+b = pydash.uniq(word_array)
+b = [(x in kjv_words) for x in b]
+unique_score = round((sum(b)/len(b)), 2)
+
+# print results.
+
+print({
+    'text': input_file.name,
+    'total_score': total_score,
+    'unique_score': unique_score
+    })
+
